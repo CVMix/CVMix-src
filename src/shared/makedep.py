a7e2945aafc2feddfcef110ca6dc669bd5e50378
Michael Levy <mike.levy.work@gmail.com>
2013-06-21 18:28:02 +0000

    One last change to the build system - both the src/ and src/shared/ Makefile use makedep.py to generate dependency files (I'm still learning python, but am pretty impressed with its flexibility -- I was expecting the script to be far more complicated if it was going to handle both the src/ and src/shared cases).
    I also reorganized the target section of both Makefiles a little bit to make it clearer what each target did, and I grouped all the .PHONY targets together at the bottom of the list.
    
    Now on to KPP!
    
    
    
    git-svn-id: https://cvmix.googlecode.com/svn/trunk@169 ec264166-b127-864d-6cb2-e100125a5c76

--- src/shared/makedep.py
+++ src/shared/makedep.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
-# usage: makedep.py $(SRC_DIR) $(OBJ_DIR) $(DEP_FILE)
+# usage: makedep.py $(DEP_FILE) $(OBJ_DIR) $(SRC_DIR) [$(SRC_DIR2)]
 
-# Read in every file in $SRC_DIR (argument 1)
-# Generate $DEP_FILE in $OBJ_DIR (argument 3 and 2, respectively)
-# If no $DEP_FILE, generates depends.d
+# Generate $DEP_FILE in $OBJ_DIR (arguments 1 and 2, respectively)
+# Read in every file in $SRC_DIR and $SRC_DIR2 (arguments 3 and 4)
+# Only depend on modules located in $SRC_DIR or $SRC_DIR2
 
 import os, sys, re
 
 try:
-  src_dir = sys.argv[1]
+  dep_file = sys.argv[1]
 except:
-  src_dir = '.'
+  dep_file = "depends.d"
 
 try:
   obj_dir = sys.argv[2]
@@ -19,24 +19,34 @@ except:
   obj_dir = '.'
 
 try:
-  dep_file = sys.argv[3]
+  src_dir = sys.argv[3]
 except:
-  dep_file = "depends.d"
+  src_dir = '.'
+
+try:
+  src_dir2 = sys.argv[4]
+except:
+  src_dir2 = src_dir
 
 fout = open(dep_file, 'w')
-files_in_src_dir = os.listdir(src_dir)
+files_in_src_dir =  os.listdir(src_dir)
+if src_dir != src_dir2:
+  files_in_src_dir.extend(os.listdir(src_dir2))
+
 for src_file in files_in_src_dir:
   file_name, file_ext = os.path.splitext(src_file)
   if file_ext == '.F90':
-    fin = open(src_dir+'/'+src_file,"r")
+    try:
+      fin = open(src_dir+'/'+src_file,"r")
+    except:
+      fin = open(src_dir2+'/'+src_file,"r")
     for line in fin:
       if re.match('^ *[Uu][Ss][Ee]',line):
         line_array = line.split()
         # statements are usually "use module, only : subroutine"
         # so we need to strip away the , to get the module name
         file_used = line_array[1].split(',')[0]
-        print file_name+'.o depends on '+file_used+'.o'
-        fout.write(obj_dir+'/'+file_name+'.o: '+obj_dir+'/'+file_used+'.o\n')
+        if file_used+'.F90' in files_in_src_dir:
+          print file_name+'.o depends on '+file_used+'.o'
+          fout.write(obj_dir+'/'+file_name+'.o: '+obj_dir+'/'+file_used+'.o\n')
     fin.close
-
-
