4b33e4b92bd813650ffcdda79bc534bc94dce448
Michael Levy <mike.levy.work@gmail.com>
2013-08-23 16:35:28 +0000

    First attempt at improving comments describing variables (also included units). Please let me know if anything is unclear or just plain wrong!
    I'm going to make sure parameters in each mixing routine are also well-commented and then I'll come back and work through renaming structure attributes (which will involve updating the put/get statements). For example, Rib will become BulkRichNumber.
    
    
    
    git-svn-id: https://cvmix.googlecode.com/svn/trunk@233 ec264166-b127-864d-6cb2-e100125a5c76

--- src/shared/cvmix_kinds_and_types.F90
+++ src/shared/cvmix_kinds_and_types.F90
@@ -57,17 +57,30 @@ module cvmix_kinds_and_types
       ! Scalar quantities
       real(cvmix_r8) :: ocn_depth, & ! distance from sea level to ocean bottom
                                      ! (positive => below sea level)
+                                     ! units: m
                         OBL_depth, & ! distance from sea level to OBL bottom
                                      ! (positive => below sea level)
+                                     ! units: m
                         surf_hgt,  & ! sea surface height
                                      ! (positive => above sea level)
+                                     ! units: m
                         surf_fric, & ! turbulent friction velocity at surface
+                                     ! units: m/s
                         surf_buoy, & ! buoyancy forcing at surface
+                                     ! units: m^2 s^-3
                         lat,       & ! latitude of column (degrees north)
+                                     ! units: can be degrees or radians (there
+                                     !        are no internal computation based
+                                     !        on this term)
                         lon,       & ! longitude of column (degrees east)
+                                     ! units: can be degrees or radians (there
+                                     !        are no internal computation based
+                                     !        on this term)
                         Coriolis,  & ! Coriolis parameter
+                                     ! units: s^-1
                         kOBL_depth   ! index of cell containing OBL (fraction
                                      ! > .5 => below cell center)
+                                     ! units: unitless
 
       ! Values on interfaces
       ! For KPP, need to store non-local transport term
@@ -81,39 +94,70 @@ module cvmix_kinds_and_types
       ! (7.128) and (7.129) of the CVMix manual.
       ! nlev+1, 4
       real(cvmix_r8), dimension(:,:), pointer :: kpp_transport_iface => NULL()
+                                             ! units: unitless (see note above)
       ! nlev+1, 2
+      ! diffusivity coefficients at interfaces (2 columns needed for double diff)
       real(cvmix_r8), dimension(:,:), pointer :: diff_iface => NULL()
+                                              ! units: m^2/s
       ! nlev+1
+      ! viscosity (momentum diffusivity) coefficients at interfaces
       real(cvmix_r8), dimension(:),   pointer :: visc_iface => NULL()
+                                              ! units: m^2/s
+      ! height of interfaces in column (positive up => most are negative)
       real(cvmix_r8), dimension(:),   pointer :: zw_iface   => NULL()
+                                              ! units: m
+      ! distance between neighboring cell centers (first value is top of ocean
+      ! to middle of first cell, last value is middle of last cell to ocean
+      ! bottom)
       real(cvmix_r8), dimension(:),   pointer :: dzw_iface  => NULL()
+                                              ! units: m
+      ! shear Richardson number at column interfaces
       real(cvmix_r8), dimension(:),   pointer :: Ri_iface   => NULL()
+                                              ! units: unitless
       ! For tidal mixing, we need the squared buoyancy frequency
       real(cvmix_r8), dimension(:),   pointer :: buoy_iface => NULL()
+                                              ! units: s^-2
 
       ! Values at tracer points
       ! nlev
+      ! Two density values are stored: the actual density of water (dens) and
+      ! the density of water after adiabatic displacement to the level below
+      ! where the water actually is (dens_lwr)
       real(cvmix_r8), dimension(:),   pointer :: dens     => NULL()
+                                              ! units: kg m^-3 
       real(cvmix_r8), dimension(:),   pointer :: dens_lwr => NULL()
+                                              ! units: kg m^-3 
+      ! height of cell centers in column (positive up => most are negative)
       real(cvmix_r8), dimension(:),   pointer :: zt       => NULL()
+                                              ! units: m
+      ! level thicknesses (positive semi-definite)
       real(cvmix_r8), dimension(:),   pointer :: dzt      => NULL()
+                                              ! units: m
+      ! bulk Richardson number
       real(cvmix_r8), dimension(:),   pointer :: Rib      => NULL()
+                                              ! units: unitless
       ! For double diffusion mixing, we need to calculate the stratification
       ! parameter R_rho. Since the denominator of this ratio may be zero,
       ! we store the numerator and denominator separately and make sure the
       ! denominator is non-zero before performing the division.
       real(cvmix_r8), dimension(:),   pointer :: strat_param_num   => NULL()
+                                              ! units: unitless
       real(cvmix_r8), dimension(:),   pointer :: strat_param_denom => NULL()
+                                              ! units: unitless
   end type cvmix_data_type
 
   ! cvmix_global_params_type contains global parameters used by multiple
   ! mixing methods.
   type, public :: cvmix_global_params_type
       integer                        :: max_nlev  ! maximum number of levels
+                                     ! units: unitless
       real(cvmix_r8)                 :: prandtl   ! Prandtl number
+                                     ! units: unitless
       ! For densities, user must keep track of units (kg/m^3 vs g/cm^3)
       real(cvmix_r8)                 :: fw_rho    ! fresh water density
+                                     ! units: kg m^-3 
       real(cvmix_r8)                 :: sw_rho    ! salt water density
+                                     ! units: kg m^-3 
   end type cvmix_global_params_type
 
 !EOP
