2e34a8dbbefef4fad44ea7b1edf20207ccc5080e
Michael Levy <mike.levy.work@gmail.com>
2014-02-24 20:52:48 -0700

    update cvmix_put_tidal

--- src/shared/cvmix_tidal.F90
+++ src/shared/cvmix_tidal.F90
@@ -98,6 +98,7 @@ contains
 ! !OUTPUT PARAMETERS:
     type(cvmix_tidal_params_type), optional, target, intent(inout) ::         &
                                               CVmix_tidal_params_user
+
 !EOP
 !BOC
 
@@ -110,54 +111,53 @@ contains
     end if
 
     if (present(mix_scheme)) then
-      call cvmix_put_tidal(CVmix_tidal_params_out, "mix_scheme",              &
-                           trim(mix_scheme))
+      call cvmix_put_tidal("mix_scheme", trim(mix_scheme),                    &
+                           CVmix_tidal_params_user)
     else
-      call cvmix_put_tidal(CVmix_tidal_params_out, "mix_scheme", "Simmons")
+      call cvmix_put_tidal("mix_scheme", "Simmons", CVmix_tidal_params_user)
     end if
 
     select case (trim(CVmix_tidal_params_out%mix_scheme))
       case ('simmons','Simmons')
         ! Unitless parameters
         if (present(efficiency)) then
-          call cvmix_put_tidal(CVmix_tidal_params_out, "efficiency",          &
-                               efficiency)
+          call cvmix_put_tidal("efficiency", efficiency,                      &
+                               CVmix_tidal_params_user)
         else
-          call cvmix_put_tidal(CVmix_tidal_params_out, "efficiency",          &
-                               0.2_cvmix_r8)
+          call cvmix_put_tidal("efficiency", 0.2_cvmix_r8,                    &
+                               CVmix_tidal_params_user)
         end if
 
         if (present(local_mixing_frac)) then
-          call cvmix_put_tidal(CVmix_tidal_params_out, "local_mixing_frac",   &
-                               local_mixing_frac)
+          call cvmix_put_tidal("local_mixing_frac", local_mixing_frac,        &
+                               CVmix_tidal_params_user)
         else
-          call cvmix_put_tidal(CVmix_tidal_params_out, "local_mixing_frac", 3)
+          call cvmix_put_tidal("local_mixing_frac", 3, CVmix_tidal_params_user)
         end if
 
         ! Parameters with units
         if (present(vertical_decay_scale)) then
-          call cvmix_put_tidal(CVmix_tidal_params_out, "vertical_decay_scale",&
-                               vertical_decay_scale)
+          call cvmix_put_tidal("vertical_decay_scale", vertical_decay_scale,  &
+                               CVmix_tidal_params_user)
         else
-          call cvmix_put_tidal(CVmix_tidal_params_out, "vertical_decay_scale",&
-                               500)
+          call cvmix_put_tidal("vertical_decay_scale", 500,                   &
+                               CVmix_tidal_params_user)
         end if
 
         if (present(depth_cutoff)) then
-          call cvmix_put_tidal(CVmix_tidal_params_out, "depth_cutoff",        &
-                               depth_cutoff)
+          call cvmix_put_tidal("depth_cutoff", depth_cutoff,                  &
+                               CVmix_tidal_params_user)
         else
           ! Default: no cutoff depth => 0 m
-          call cvmix_put_tidal(CVmix_tidal_params_out, "depth_cutoff",        &
-                               cvmix_zero)
+          call cvmix_put_tidal("depth_cutoff", 0, CVmix_tidal_params_user)
         end if
 
         if (present(max_coefficient)) then
-          call cvmix_put_tidal(CVmix_tidal_params_out, "max_coefficient",     &
-                               max_coefficient)
+          call cvmix_put_tidal("max_coefficient", max_coefficient,            &
+                               CVmix_tidal_params_user)
         else
-          call cvmix_put_tidal(CVmix_tidal_params_out, "max_coefficient",     &
-                               50e-4_cvmix_r8)
+          call cvmix_put_tidal("max_coefficient", 50e-4_cvmix_r8,             &
+                               CVmix_tidal_params_user)
         end if
 
       case DEFAULT
@@ -304,10 +304,10 @@ contains
 
 !BOP
 
-! !IROUTINE: cvmix_put_tidal_real
+! !IROUTINE: cvmix_put_tidal_int
 ! !INTERFACE:
 
-  subroutine cvmix_put_tidal_int(CVmix_tidal_params, varname, val)
+  subroutine cvmix_put_tidal_int(varname, val, CVmix_tidal_params_user)
 
 ! !DESCRIPTION:
 !  Write an integer value into a cvmix\_tidal\_params\_type variable.
@@ -322,12 +322,13 @@ contains
     integer,          intent(in) :: val
 
 ! !OUTPUT PARAMETERS:
-    type(cvmix_tidal_params_type), intent(inout) :: CVmix_tidal_params
+    type(cvmix_tidal_params_type), optional, target, intent(inout) ::         &
+                                              CVmix_tidal_params_user
 
 !EOP
 !BOC
 
-    call cvmix_put_tidal(CVmix_tidal_params, varname, real(val,cvmix_r8))
+    call cvmix_put_tidal(varname, real(val,cvmix_r8), CVmix_tidal_params_user)
 
 !EOC
 
@@ -338,7 +339,7 @@ contains
 ! !IROUTINE: cvmix_put_tidal_real
 ! !INTERFACE:
 
-  subroutine cvmix_put_tidal_real(CVmix_tidal_params, varname, val)
+  subroutine cvmix_put_tidal_real(varname, val, CVmix_tidal_params_user)
 
 ! !DESCRIPTION:
 !  Write a real value into a cvmix\_tidal\_params\_type variable.
@@ -353,21 +354,31 @@ contains
     real(cvmix_r8),   intent(in) :: val
 
 ! !OUTPUT PARAMETERS:
-    type(cvmix_tidal_params_type), intent(inout) :: CVmix_tidal_params
+    type(cvmix_tidal_params_type), optional, target, intent(inout) ::         &
+                                              CVmix_tidal_params_user
+
 !EOP
 !BOC
 
+    type(cvmix_tidal_params_type), pointer :: CVmix_tidal_params_out
+
+    if (present(CVmix_tidal_params_user)) then
+      CVmix_tidal_params_out => CVmix_tidal_params_user
+    else
+      CVmix_tidal_params_out => CVmix_tidal_params_saved
+    end if
+
     select case (trim(varname))
       case ('efficiency')
-        CVmix_tidal_params%efficiency = val
+        CVmix_tidal_params_out%efficiency = val
       case ('vertical_decay_scale')
-        CVmix_tidal_params%vertical_decay_scale = val
+        CVmix_tidal_params_out%vertical_decay_scale = val
       case ('max_coefficient')
-        CVmix_tidal_params%max_coefficient = val
+        CVmix_tidal_params_out%max_coefficient = val
       case ('local_mixing_frac')
-        CVmix_tidal_params%local_mixing_frac = val
+        CVmix_tidal_params_out%local_mixing_frac = val
       case ('depth_cutoff')
-        CVmix_tidal_params%depth_cutoff = val
+        CVmix_tidal_params_out%depth_cutoff = val
       case DEFAULT
         print*, "ERROR: ", trim(varname), " not a valid choice!"
         stop 1
@@ -383,7 +394,7 @@ contains
 ! !IROUTINE: cvmix_put_tidal_str
 ! !INTERFACE:
 
-  subroutine cvmix_put_tidal_str(CVmix_tidal_params, varname, val)
+  subroutine cvmix_put_tidal_str(varname, val, CVmix_tidal_params_user)
 
 ! !DESCRIPTION:
 !  Write a string into a cvmix\_tidal\_params\_type variable.
@@ -398,13 +409,23 @@ contains
     character(len=*), intent(in) :: val
 
 ! !OUTPUT PARAMETERS:
-    type(cvmix_tidal_params_type), intent(inout) :: CVmix_tidal_params
+    type(cvmix_tidal_params_type), optional, target, intent(inout) ::         &
+                                              CVmix_tidal_params_user
+
 !EOP
 !BOC
 
+    type(cvmix_tidal_params_type), pointer :: CVmix_tidal_params_out
+
+    if (present(CVmix_tidal_params_user)) then
+      CVmix_tidal_params_out => CVmix_tidal_params_user
+    else
+      CVmix_tidal_params_out => CVmix_tidal_params_saved
+    end if
+
     select case (trim(varname))
       case ('mix_scheme')
-        CVmix_tidal_params%mix_scheme = val
+        CVmix_tidal_params_out%mix_scheme = val
       case DEFAULT
         print*, "ERROR: ", trim(varname), " not a valid choice!"
         stop 1
