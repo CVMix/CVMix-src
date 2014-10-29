89fa4a02c37e2c0b80f5c76d362f17246f061410
Michael Levy <mike.levy.work@gmail.com>
2014-10-29 17:29:09 -0600

    Left a file off last commit

--- src/drivers/cvmix_shear_drv.F90
+++ src/drivers/cvmix_shear_drv.F90
@@ -45,31 +45,59 @@ Subroutine cvmix_shear_driver(nlev, max_nlev)
   integer, parameter :: ncol = 6
 
   ! CVMix datatypes
-  type(cvmix_data_type), dimension(ncol) :: CVmix_vars_PP
-  type(cvmix_data_type)                  :: CVmix_vars_LMD
+  type(cvmix_data_type)                  :: CVmix_vars_LMD_1D, CVmix_vars_PP_1D
+  type(cvmix_data_type), dimension(ncol) :: CVmix_vars_PP_2D
 
-  ! Both regression tests look at Richardson numbers in [0,1]
+  ! All regression tests look at Richardson numbers in [0,1]
   real(cvmix_r8), dimension(:), allocatable, target :: Ri_g
 
-  ! LMD test is single column, PP test is multi-column
-  real(cvmix_r8), dimension(:),   allocatable, target :: Mdiff_1D, Tdiff_1D
+  ! "1D" variables will be 2 x nlev+1 (1 for LMD, 1 for PP)
+  ! "2D" variables will be ncol x nlev+1 (for PP)
+  real(cvmix_r8), dimension(:,:), allocatable, target :: Mdiff_1D, Tdiff_1D
   real(cvmix_r8), dimension(:,:), allocatable, target :: Mdiff_2D, Tdiff_2D
 
   ! Hard-code in parameters for each c in Table 1
   real(cvmix_r8), dimension(ncol) :: PP_nu_zero_2D, PP_exp_2D, PP_alpha_2D
-!  real(cvmix_r8)                  :: PP_nu_zero, PP_exp, PP_alpha
+  real(cvmix_r8)                  :: PP_nu_zero, PP_exp, PP_alpha
 
   integer :: icol, kw, fid
 
   ! Namelist variables
   ! KPP mixing parameters for column
-  real(cvmix_r8) :: KPP_nu_zero, KPP_Ri_zero, KPP_exp
+  real(cvmix_r8) :: LMD_nu_zero, LMD_Ri_zero, LMD_exp
 
-  ! Namelist with shear mixing parameters
-  namelist/KPP_nml/KPP_nu_zero, KPP_Ri_zero, KPP_exp
+  namelist/LMD_nml/LMD_nu_zero, LMD_Ri_zero, LMD_exp
+  namelist/PP_nml/PP_nu_zero, PP_alpha, PP_exp
 
   print*, "Active levels: ", nlev
   print*, "Levels allocated in memory: ", max_nlev
+
+  ! Namelist (set defaults then read from file)
+  LMD_nu_zero = real(5e-3, cvmix_r8)
+  LMD_Ri_zero = real(0.7,  cvmix_r8)
+  LMD_exp     = real(3.0,  cvmix_r8)
+
+  PP_nu_zero = real(5e-3, cvmix_r8)
+  PP_alpha   = real(5.0,  cvmix_r8)
+  PP_exp     = real(2.0,  cvmix_r8)
+
+  read(*, nml=LMD_nml)
+  read(*, nml=PP_nml)
+
+  print*, ""
+  print*, "Parameters Used in LMD test"
+  print*, "----"
+  print*, "KPP_nu_zero = ", LMD_nu_zero
+  print*, "KPP_Ri_zero = ", LMD_Ri_zero
+  print*, "KPP_exp = ", LMD_exp
+
+  print*, ""
+  print*, "Parameters Used in PP test"
+  print*, "----"
+  print*, "PP_nu_zero = ", PP_nu_zero
+  print*, "PP_alpha = ", PP_alpha
+  print*, "PP_exp = ", PP_exp
+
 
   ! Ri_g should increase from 0 to 1 in active portion of level
   allocate(Ri_g(max_nlev+1))
@@ -79,7 +107,7 @@ Subroutine cvmix_shear_driver(nlev, max_nlev)
   end do
 
   ! Allocate memory to store viscosity and diffusivity values
-  allocate(Mdiff_1D(max_nlev+1), Tdiff_1D(max_nlev+1))
+  allocate(Mdiff_1D(2,max_nlev+1), Tdiff_1D(2,max_nlev+1))
   allocate(Mdiff_2D(ncol,max_nlev+1), Tdiff_2D(ncol,max_nlev+1))
 
   ! Set Pacanowski-Philander coefficients
@@ -90,39 +118,57 @@ Subroutine cvmix_shear_driver(nlev, max_nlev)
   PP_exp_2D(4)   = real(1.0,cvmix_r8)
   PP_alpha_2D(5) = real(10.0,cvmix_r8)
 
-  ! Initialization for CVMix data type
-  call cvmix_put(CVmix_vars_LMD, 'nlev',     nlev)
-  call cvmix_put(CVmix_vars_LMD, 'max_nlev', max_nlev)
+  ! Initialization for LMD test
+  call cvmix_put(CVmix_vars_LMD_1D, 'nlev',     nlev)
+  call cvmix_put(CVmix_vars_LMD_1D, 'max_nlev', max_nlev)
   ! Point CVmix_vars values to memory allocated above
-  CVmix_vars_LMD%Mdiff_iface           => Mdiff_1D
-  CVmix_vars_LMD%Tdiff_iface           => Tdiff_1D
-  CVmix_vars_LMD%ShearRichardson_iface => Ri_g
+  CVmix_vars_LMD_1D%Mdiff_iface           => Mdiff_1D(1,:)
+  CVmix_vars_LMD_1D%Tdiff_iface           => Tdiff_1D(1,:)
+  CVmix_vars_LMD_1D%ShearRichardson_iface => Ri_g
 
-  ! Read / set KPP parameters
-  read(*, nml=KPP_nml)
-  call cvmix_init_shear(mix_scheme='KPP', KPP_nu_zero=KPP_nu_zero,            &
-                        KPP_Ri_zero=KPP_Ri_zero, KPP_exp=KPP_exp)
-  call cvmix_coeffs_shear(CVmix_vars_LMD)
+  ! Initialization for 1D PP test
+  call cvmix_put(CVmix_vars_PP_1D, 'nlev',     nlev)
+  call cvmix_put(CVmix_vars_PP_1D, 'max_nlev', max_nlev)
+  ! Point CVmix_vars values to memory allocated above
+  CVmix_vars_PP_1D%Mdiff_iface           => Mdiff_1D(2,:)
+  CVmix_vars_PP_1D%Tdiff_iface           => Tdiff_1D(2,:)
+  CVmix_vars_PP_1D%ShearRichardson_iface => Ri_g
+
+  ! Initialization for 2D PP test
   do icol=1,ncol
-      call cvmix_put(CVmix_vars_PP(icol), 'nlev',     nlev)
-      call cvmix_put(CVmix_vars_PP(icol), 'max_nlev', max_nlev)
-      CVmix_vars_PP(icol)%Mdiff_iface           => Mdiff_2D(icol,:)
-      CVmix_vars_PP(icol)%Tdiff_iface           => Tdiff_2D(icol,:)
-      CVmix_vars_PP(icol)%ShearRichardson_iface => Ri_g
-      call cvmix_init_shear(mix_scheme='PP', PP_nu_zero=PP_nu_zero_2D(icol),  &
-                            PP_alpha=PP_alpha_2D(icol), PP_exp=PP_exp_2D(icol))
-      call cvmix_coeffs_shear(CVmix_vars_PP(icol))
+    call cvmix_put(CVmix_vars_PP_2D(icol), 'nlev',     nlev)
+    call cvmix_put(CVmix_vars_PP_2D(icol), 'max_nlev', max_nlev)
+    CVmix_vars_PP_2D(icol)%Mdiff_iface           => Mdiff_2D(icol,:)
+    CVmix_vars_PP_2D(icol)%Tdiff_iface           => Tdiff_2D(icol,:)
+    CVmix_vars_PP_2D(icol)%ShearRichardson_iface => Ri_g
+  end do
+
+  ! Set LMD94 parameters
+  call cvmix_init_shear(mix_scheme='KPP', KPP_nu_zero=LMD_nu_zero,            &
+                        KPP_Ri_zero=LMD_Ri_zero, KPP_exp=LMD_exp)
+  call cvmix_coeffs_shear(CVmix_vars_LMD_1D)
+
+  ! Set PP81 single column parameters
+  call cvmix_init_shear(mix_scheme='PP', PP_nu_zero=PP_nu_zero,               &
+                        PP_alpha=PP_alpha, PP_exp=PP_exp)
+  call cvmix_coeffs_shear(CVmix_vars_PP_1D)
+
+  ! Set PP81 multiple column
+  do icol=1,ncol
+    call cvmix_init_shear(mix_scheme='PP', PP_nu_zero=PP_nu_zero_2D(icol),    &
+                          PP_alpha=PP_alpha_2D(icol), PP_exp=PP_exp_2D(icol))
+    call cvmix_coeffs_shear(CVmix_vars_PP_2D(icol))
   end do
 
   ! Output
   ! (1) LMD column
 #ifdef _NETCDF
-  call cvmix_io_open(fid, "data.nc", "nc")
+  call cvmix_io_open(fid, "data_LMD.nc", "nc")
 #else
-  call cvmix_io_open(fid, "data.out", "ascii")
+  call cvmix_io_open(fid, "data_LMD.out", "ascii")
 #endif
 
-  call cvmix_output_write(fid, CVmix_vars_LMD, (/"Ri   ", "Tdiff"/))
+  call cvmix_output_write(fid, CVmix_vars_LMD_1D, (/"Ri   ", "Tdiff"/))
 #ifdef _NETCDF
   call cvmix_output_write_att(fid, "long_name", "Richardson number",          &
                               var_name="ShearRichardson")
@@ -134,13 +180,32 @@ Subroutine cvmix_shear_driver(nlev, max_nlev)
 #endif
   call cvmix_io_close(fid)
 
-  ! (2) PP columns
+  ! (2) PP single column
 #ifdef _NETCDF
-  call cvmix_io_open(fid, "data_PP.nc", "nc")
+  call cvmix_io_open(fid, "data_PP1d.nc", "nc")
 #else
-  call cvmix_io_open(fid, "data_PP.out", "ascii")
+  call cvmix_io_open(fid, "data_PP1d.out", "ascii")
 #endif
-  call cvmix_output_write(fid, CVmix_vars_PP, (/"Ri   ", "Tdiff"/))
+
+  call cvmix_output_write(fid, CVmix_vars_PP_1D, (/"Ri   ", "Tdiff"/))
+#ifdef _NETCDF
+  call cvmix_output_write_att(fid, "long_name", "Richardson number",          &
+                              var_name="ShearRichardson")
+  call cvmix_output_write_att(fid, "units", "unitless",                       &
+                              var_name="ShearRichardson")
+  call cvmix_output_write_att(fid, "long_name", "temperature diffusivity",    &
+                              var_name="Tdiff")
+  call cvmix_output_write_att(fid, "units", "m^2/s", var_name="Tdiff")
+#endif
+  call cvmix_io_close(fid)
+
+  ! (3) PP multiple columns
+#ifdef _NETCDF
+  call cvmix_io_open(fid, "data_PP2d.nc", "nc")
+#else
+  call cvmix_io_open(fid, "data_PP2d.out", "ascii")
+#endif
+  call cvmix_output_write(fid, CVmix_vars_PP_2D, (/"Ri   ", "Tdiff"/))
 #ifdef _NETCDF
   call cvmix_output_write_att(fid, "long_name", "Richardson number",          &
                               var_name="ShearRichardson")
