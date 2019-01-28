13866b86c259942087c4afe04de3221b741c7ca0
Qing Li <lqingpku@gmail.com>
2019-01-27 21:32:45 -0700

    Clean up langmuir driver

--- src/cvmix_driver.F90
+++ src/cvmix_driver.F90
@@ -49,8 +49,6 @@ Program cvmix_driver
       call cvmix_ddiff_driver(2*nlev, 2*max_nlev)
     case ('kpp')
       call cvmix_kpp_driver()
-    case ('kpp-langmuir')
-      call cvmix_kpp_langmuir_driver()
     case DEFAULT
       print*, "WARNING: mix_type = '", trim(mix_type), "' is not supported by this driver."
   end select
